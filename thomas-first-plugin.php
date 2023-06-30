3aba1bc223ca5ea0b8f68f1899d53730a17d149e
ThomF <seniorswanky@icloud.com>
2023-06-29 22:10:42 -0600

    v

--- thomas-first-plugin.php
+++ thomas-first-plugin.php
@@ -1,23 +1,23 @@
 <?php
-
+$php = "i am silly";
 /*
  * Plugin Name: Thomas First Plugin
  */
 
- function wpb_follow_us($content) {
+ function wpb_follow_us($asdf) {
  
     // Only do this when a single post is displayed
     if ( is_single() ) { 
         echo "HELLO THERE";
-        echo "i am phping"
+        echo $php;
     // Message you want to display after the post
     // Add URLs to your own Twitter and Facebook profiles
      
-    $content .= '<p class="follow-us">If you liked this article, then please follow us on <a href="http://twitter.com/wpbeginner" title="WPBeginner on Twitter" target="_blank" rel="nofollow">Twitter</a> and <a href="https://www.facebook.com/wpbeginner" title="WPBeginner on Facebook" target="_blank" rel="nofollow">Facebook</a>.</p>';
+    $asdf .= '<p class="follow-us">If you liked this article, then please follow us on <a href="http://twitter.com/wpbeginner" title="WPBeginner on Twitter" target="_blank" rel="nofollow">Twitter</a> and <a href="https://www.facebook.com/wpbeginner" title="WPBeginner on Facebook" target="_blank" rel="nofollow">Facebook</a>.</p>';
      
     } 
     // Return the content
-    return $content; 
+    return $asdf; 
      
     }
     // Hook our function to WordPress the_content filter
